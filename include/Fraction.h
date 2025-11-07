7a2aa4f991a39ce58d6bf33c511d2e2ec4b5b60b
ksrcr7 <kasrabakhshaei25@gmail.com>
2025-11-07 23:30:53 +0330

    Initial commit (sources only)

---
+++ include/Fraction.h
@@ -0,0 +1,32 @@
+
+
+#ifndef FRACTION_CALCULATOR_FRACTION_H
+#define FRACTION_CALCULATOR_FRACTION_H
+
+#include <iostream>
+
+
+class Fraction{
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif //FRACTION_CALCULATOR_FRACTION_H
